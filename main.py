e704d1962fa1955876f21a9d55dab0c295681345
Radi Aman Riyas <radi.riyas@ghaf.ai>
2024-11-02 19:01:20 +0400

    feat: implement lexer and parser funccs
    - add TokenType and Lexeme classes
    - implement tokenize()
    - set up tokenizer generator
    - clear output file on startup

--- main.py
+++ main.py
@@ -1,119 +1,254 @@
 import os
+from enum import Enum, auto
+
+class TokenType(Enum):
+    """
+    Enum class to represent token types for this Prolog grammar
+    """
+    PLUS = auto()               # +
+    MINUS = auto()              # -
+    MULTIPLY = auto()           # *
+    DIVIDE = auto()             # /
+    ESCAPE = auto()             # \
+    CARET = auto()              # ^
+    TILDE = auto()              # ~
+    COLON = auto()              # :-
+    PERIOD = auto()             # .
+    COMMA = auto()              # ,
+    OPEN_PAREN = auto()         # (
+    CLOSE_PAREN = auto()        # )
+    QUERY = auto()              # ?-
+
+    ATOM = auto()               # Atoms (here we'll have `criminal`, `american`, etc.)
+    VARIABLE = auto()
+    NUMERAL = auto()
+
+    EOF = auto()
+    INVALID = auto()            # Other misc. chars go here
+
+class Lexeme:
+    """
+    Class representing a Lexeme
+    """
+    def __init__(self, token_type, value, line, char_position):
+        self.token_type = token_type
+        self.value = value
+        self.line = line
+        self.char_position = char_position
+
+    def __str__(self):
+        return f"Lexeme({self.token_type}, '{self.value}', Line: {self.line}, Char: {self.char_position})"
+
+
 class Lexer:
-    def __init__(self, code):
-        self.code = code.splitlines()
-        self.tokens = []
-        self.current_line = 0
-        self.current_char = 0
-        self.tokenize()
+    def __init__(self, code: str):
+        self.code = code
+        self.position = 0
+        self.line = 1
+        self.char_position = 0
+        self.length = len(code)
 
-    # @TODO
+    def get_next_char(self):
+        if self.position >= self.length:
+            return None
+        char = self.code[self.position]
+        self.position += 1
+        self.char_position += 1
+        if char == '\n':
+            self.line += 1
+            self.char_position = 0
+        return char
+
+    def peek_next_char(self):
+        if self.position >= self.length:
+            return None
+        return self.code[self.position]
+
     def tokenize(self):
-        pass
+        while True:
+            char = self.get_next_char()
+            if char is None:
+                yield Lexeme(TokenType.EOF, "EOF", self.line, self.char_position)
+                break
 
-    # @TODO:
-    def get_next_token(self):
-        pass
+            if char.isspace():
+                continue
 
-    # @TODO:
-    def peek_token(self):
-        pass
+            elif char == '+':
+                yield Lexeme(TokenType.PLUS, char, self.line, self.char_position)
+            elif char == '-':
+                yield Lexeme(TokenType.MINUS, char, self.line, self.char_position)
+            elif char == '*':
+                yield Lexeme(TokenType.MULTIPLY, char, self.line, self.char_position)
+            elif char == '/':
+                yield Lexeme(TokenType.DIVIDE, char, self.line, self.char_position)
+            elif char == '\\':
+                yield Lexeme(TokenType.ESCAPE, char, self.line, self.char_position)
+            elif char == '^':
+                yield Lexeme(TokenType.CARET, char, self.line, self.char_position)
+            elif char == '~':
+                yield Lexeme(TokenType.TILDE, char, self.line, self.char_position)
+            elif char == ':':
+                if self.peek_next_char() == '-':
+                    yield Lexeme(TokenType.COLON, ":-", self.line, self.char_position)
+                    self.get_next_char()
+                else:
+                    yield Lexeme(TokenType.COLON, ":", self.line, self.char_position)
+            elif char == '?':
+                if self.peek_next_char() == '-':
+                    yield Lexeme(TokenType.QUERY, "?-", self.line, self.char_position)
+                    self.get_next_char()
+                else:
+                    yield Lexeme(TokenType.INVALID, char, self.line, self.char_position)
+            elif char == '.':
+                yield Lexeme(TokenType.PERIOD, char, self.line, self.char_position)
+            elif char == ',':
+                yield Lexeme(TokenType.COMMA, char, self.line, self.char_position)
+            elif char == '(':
+                yield Lexeme(TokenType.OPEN_PAREN, char, self.line, self.char_position)
+            elif char == ')':
+                yield Lexeme(TokenType.CLOSE_PAREN, char, self.line, self.char_position)
+            elif char.isdigit():
+                yield self.tokenize_numeral(char)
+            elif char.islower():
+                yield self.tokenize_atom(char)
+            elif char.isupper() or char == '_':
+                yield self.tokenize_variable(char)
+            else:
+                yield Lexeme(TokenType.INVALID, char, self.line, self.char_position)
+
+    def tokenize_numeral(self, char):
+        start_position = self.char_position
+        value = char
+        while self.peek_next_char() and self.peek_next_char().isdigit():
+            value += self.get_next_char()
+        return Lexeme(TokenType.NUMERAL, value, self.line, start_position)
+
+    def tokenize_atom(self, char):
+        start_position = self.char_position
+        value = char
+        while self.peek_next_char() and (self.peek_next_char().isalnum() or self.peek_next_char() == '_'):
+            value += self.get_next_char()
+        return Lexeme(TokenType.ATOM, value, self.line, start_position)
+
+    def tokenize_variable(self, char):
+        start_position = self.char_position
+        value = char
+        while self.peek_next_char() and (self.peek_next_char().isalnum() or self.peek_next_char() == '_'):
+            value += self.get_next_char()
+        return Lexeme(TokenType.VARIABLE, value, self.line, start_position)
 
 
 class Parser:
     def __init__(self, lexer):
-        self.lexer = lexer
+        self.tokens = lexer.tokenize()
         self.current_token = None
         self.errors = []
         self.advance()
 
     def advance(self):
-        self.current_token = self.lexer.get_next_token()
+        try:
+            self.current_token = next(self.tokens)
+        except StopIteration:
+            self.current_token = Lexeme(TokenType.EOF, "EOF", -1, -1)
+
+    def match(self, expected_type, ):
+        if self.current_token.token_type == expected_type:
+            self.advance()
+        else:
+            self.error(f"Expected {expected_type}, but found {self.current_token.token_type}")
 
     def parse(self):
         self.program()
+        if self.current_token.token_type != TokenType.EOF:
+            self.error("Expected end of file")
 
-    # @TODO: change to enums QUERY, CLAUSE_LIST, etc ?
     def program(self):
-        if self.current_token == "query":
+        if self.current_token.token_type == TokenType.QUERY:
             self.query()
-        elif self.current_token == "clause-list":
+        elif self.current_token.token_type == TokenType.ATOM:
             self.clause_list()
-            self.query()
+            if self.current_token.token_type == TokenType.QUERY:
+                self.query()
+            elif self.current_token.token_type != TokenType.EOF:
+                self.error("Expected end of file after clause list")
         else:
             self.error("Expected <clause-list> or <query> at start of program")
 
-    # @TODO:
-    def clause_list(self):
-        pass
-
-    # @TODO:
     def clause(self):
-        pass
+        self.predicate()
+        if self.current_token.token_type == TokenType.PERIOD:
+            self.advance()
+        elif self.current_token.token_type == TokenType.COLON:
+            self.advance()
+            self.predicate_list()
+            self.match(TokenType.PERIOD)
+        else:
+            self.error("Expected '.' or ':-' after predicate")
 
-    # @TODO:
+    def clause_list(self):
+        self.clause()
+        if self.current_token.token_type == TokenType.ATOM:
+            self.clause_list()
+
     def query(self):
-        pass
+        if self.current_token.token_type == TokenType.QUERY:
+            self.advance()
+            self.predicate_list()
+            if self.current_token.token_type == TokenType.PERIOD:
+                self.advance()
+            else:
+                self.error("Expected '.' at the end of query")
+        else:
+            self.error("Expected '?-' to start a query")
 
-    # @TODO:
     def predicate_list(self):
         self.predicate()
-        while self.current_token == ",":
+        while self.current_token.token_type == TokenType.COMMA:
             self.advance()
             self.predicate()
 
-    def predicate(self):
-        if self.is_atom():
-            self.advance()
-            if self.current_token == "(":
-                self.advance()
-                self.term_list()
-                if self.current_token == ")":
-                    self.advance()
-                else:
-                    self.error("Expected ')' after term list")
-        else:
-            self.error("Expected <atom> in predicate")
-
     def term_list(self):
         self.term()
-        while self.current_token == ",":
+        while self.current_token.token_type == TokenType.COMMA:
             self.advance()
             self.term()
 
     def term(self):
-        if self.is_atom() or self.is_variable() or self.is_structure() or self.is_numeral():
+        if self.current_token.token_type == TokenType.ATOM:
+            self.advance()
+            if self.current_token.token_type == TokenType.OPEN_PAREN:
+                self.advance()
+                self.term_list()
+                self.match(TokenType.CLOSE_PAREN)
+        elif self.current_token.token_type in {TokenType.VARIABLE, TokenType.NUMERAL}:
             self.advance()
         else:
             self.error("Expected <term>")
 
-    # @TODO:
-    def is_atom(self):
-        pass
+    def predicate(self):
+        if self.current_token.token_type == TokenType.ATOM:
+            self.advance()
+            if self.current_token.token_type == TokenType.OPEN_PAREN:
+                self.advance()
+                self.term_list()
+                self.match(TokenType.CLOSE_PAREN)
+        else:
+            self.error("Expected <atom> in predicate")
 
-    # @TODO:
-    def is_variable(self):
-        pass
-
-    # @TODO:
-    def is_structure(self):
-        pass
-
-    # @TODO:
-    def is_numeral(self):
-        pass
-
-    def error(self, message):
-        error_msg = f"Syntax Error: {message} at line {self.lexer.current_line}, char {self.lexer.current_char}"
+    def error(self, message, ):
+        error_msg = f"Syntax Error: {message} at line {self.current_token.line}, char {self.current_token.char_position}"
         self.errors.append(error_msg)
-        self.advance()
+        print(error_msg)
 
 def main():
     file_num = 1
-    output = []
+    output_filename = "parser_output.txt"
+
+    open(output_filename, "w").close()
 
     while True:
-        filename = f"{file_num}.txt"
+        filename = f"files/{file_num}.txt"
         if not os.path.isfile(filename):
             break
 
@@ -122,18 +257,14 @@ def main():
 
         lexer = Lexer(code)
         parser = Parser(lexer)
-
         parser.parse()
 
-        if parser.errors:
-            output.append(f"{filename} contains syntax errors:\n" + "\n".join(parser.errors))
-        else:
-            output.append(f"{filename} is syntactically correct")
-
+        with open(output_filename, "a") as f:
+            if parser.errors:
+                f.write(f"{filename} contains syntax errors:\n" + "\n".join(parser.errors) + "\n\n")
+            else:
+                f.write(f"{filename} is syntactically correct\n\n")
         file_num += 1
-
-    with open("parser_output.txt", "w") as f:
-        f.write("\n\n".join(output))
 
 
 if __name__ == "__main__":
