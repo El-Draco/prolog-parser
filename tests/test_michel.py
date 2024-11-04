fd5e8a352549cc0b5c8a4d43de1577b7e2eead29
Radi Aman Riyas <radi.riyas@ghaf.ai>
2024-11-04 19:13:57 +0400

    feat: mandatory test cases

--- tests/test_michel.py
+++ tests/test_michel.py
@@ -1,2 +1,88 @@
-#@TODO: setup individual unit tests for each of the 6 programs michel provided
-#       and add a rule to the makefile 'michel' to only run tests from this file (set as default)
+from main import Lexer, Parser
+import os
+
+def test_file1():
+    filename = "../files/1.txt"
+    assert os.path.isfile(filename)
+
+
+    with open(filename, 'r') as f:
+        code = f.read()
+
+        lexer = Lexer(code)
+        parser = Parser(lexer)
+        parser.parse()
+
+        assert parser.errors == []
+
+def test_file2():
+    filename = "../files/2.txt"
+    assert os.path.isfile(filename)
+
+
+    with open(filename, 'r') as f:
+        code = f.read()
+
+        lexer = Lexer(code)
+        parser = Parser(lexer)
+        parser.parse()
+
+        assert parser.errors == []
+
+def test_file3():
+    filename = "../files/3.txt"
+    assert os.path.isfile(filename)
+
+
+    with open(filename, 'r') as f:
+        code = f.read()
+
+        lexer = Lexer(code)
+        parser = Parser(lexer)
+        parser.parse()
+
+        assert parser.errors == []
+
+def test_file4():
+    filename = "../files/4.txt"
+    assert os.path.isfile(filename)
+
+
+    with open(filename, 'r') as f:
+        code = f.read()
+
+        lexer = Lexer(code)
+        parser = Parser(lexer)
+        parser.parse()
+
+        assert len(parser.errors) > 0
+
+def test_file5():
+    filename = "../files/5.txt"
+    assert os.path.isfile(filename)
+
+
+    with open(filename, 'r') as f:
+        code = f.read()
+
+        lexer = Lexer(code)
+        parser = Parser(lexer)
+        parser.parse()
+
+        assert len(parser.errors) > 0
+
+
+def test_file6():
+    filename = "../files/6.txt"
+    assert os.path.isfile(filename)
+
+
+    with open(filename, 'r') as f:
+        code = f.read()
+
+        lexer = Lexer(code)
+        parser = Parser(lexer)
+        parser.parse()
+
+        assert len(parser.errors) > 0
+
